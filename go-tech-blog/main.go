1d69fe2f884a319b873363649ee879ff92d0f82f
nakashima1125 <free.nakashima@gmail.com>
2021-09-16 11:55:35 +0900

    a

--- go-tech-blog/main.go
+++ go-tech-blog/main.go
@@ -45,7 +45,6 @@ func articleIndex(c echo.Context) error {
 	      func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
 	        return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
 	      }
-	    
 	      func render(c echo.Context, file string, data map[string]interface{}) error {
 	        b, err := htmlBlob(file, data)
 	        if err != nil {
