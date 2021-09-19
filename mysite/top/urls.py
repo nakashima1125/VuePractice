e32a97eff04d257dc43fb8a8f6787761e30b7d86
nakashima1125 <free.nakashima@gmail.com>
2021-09-20 01:38:42 +0900

    aa

--- mysite/top/urls.py
+++ mysite/top/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'top'
 urlpatterns = [
     path('index/', views.IndexView.as_view(), name='index'),
-    path('mm/', views.IndexView.as_view(), name='mm'),
     path('', views.TopView.as_view(), name='top'),
 ]
 
--- mysite/top/views.py
+++ mysite/top/views.py
@@ -1,9 +1,7 @@
 from django.views.generic import TemplateView
-from django.http import HttpResponse
 
 class IndexView(TemplateView):
   template_name = 'index.html'
-    #template_nameに表示するテンプレートファイルのパスを指定
 
 class TopView(TemplateView):
   template_name = 'top.html'
