691900b86f84b25f2361ef28a22cd77923314038
nakashima1125 <free.nakashima@gmail.com>
2022-05-28 19:18:31 +0900

    air commit

---
+++ java_test/Test2.java
@@ -0,0 +1,11 @@
+package java_test;
+
+public class Test2 {
+  String name;
+  int age;
+
+  Test2(String name, int age){
+    this.name = name;
+    this.age = age;
+  }
+}
---
+++ java_test/Test2Use.java
@@ -0,0 +1,19 @@
+package java_test;
+import java.util.*;
+
+public class Test2Use {
+  public static void main(String[] args) {
+    Scanner scan1 = new Scanner(System.in);
+    Scanner scan2 = new Scanner(System.in);
+
+    System.out.print("名前の初期値を入力して下さい:");
+    String s = scan1.nextLine();
+
+    System.out.print("年齢の初期値を入力して下さい:");
+    int num = scan2.nextInt();
+
+    System.out.println("クラスの初期値をします");
+    Test2 u = new Test2(s, num);
+    System.out.println(u.name + "さんの年齢は" + u.age + "歳です");
+  }
+}
