6813aebeeec84a14d022928fefac3aaefad56681
nakashima1125 <free.nakashima@gmail.com>
2022-05-28 19:32:32 +0900

    air commit

---
+++ java_test/Test3.java
@@ -0,0 +1,17 @@
+package java_test;
+
+public class Test3 {
+  String name;
+  int age;
+
+  Test3(String name, int age) {
+    this();
+    this.name = name;
+    this.age = age;
+  }
+  
+  Test3() {
+    this.name = "中島 太郎";
+    this.age = 20;
+  }
+}
