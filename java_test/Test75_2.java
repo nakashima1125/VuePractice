dc1d9f56543c5d7a81f4bc94d8f5ae8d2aeb2c87
nakashima1125 <free.nakashima@gmail.com>
2022-05-29 15:31:55 +0900

    air push

---
+++ java_test/Test75_2.java
@@ -0,0 +1,11 @@
+package java_test;
+
+public class Test75_2 {
+  String productName;
+  int price;
+
+  Test75_2(String productName, int price) {
+    this.productName = productName;
+    this.price = price;
+  }
+}
