ed25738f4cf425101fc52a7e1ed36d4fec6e56df
nakashima1125 <nakashima1125@gmail.com>
2022-05-31 16:33:08 +0900

    temp make regression

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from pkg_resources import safe_extra
+import numpy as np
 
 
 def linear_regression(train_X, train_Y):
-    pass
+    tmp = np.args(train_X.T @ train_X) @ (train_X.T @ train_Y)
+    pridict_Y =
 
 
 if __name__ == '__main__':
     data = pd.DataFrame({"temprature": [4, 10, 6, 11, 12, 13, 20, 13, 24, 26, 12, 18, 22, 18, 20, 23, 26, 28],
                          "sales": [30, 35, 28, 38, 35, 40, 60, 34, 63, 65, 38, 40, 41, 43, 42, 55, 65, 69]})
 
-    train_X = data["temprature"]
-    train_Y = data["sales"]
+    print(data["temprature"].values.shape)
+    train_X = np.concatenate(
+        (np.ones((18, 1)), data["temprature"].values[:, None]), axis=1)
+    train_Y = data["sales"].values
 #   pridict_Y = linear_regression(train_X, train_Y)
 
 #   print("予測データ", pridict_Y.shape)
