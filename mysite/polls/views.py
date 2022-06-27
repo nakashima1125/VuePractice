7a6a3e9e32fe28ea5840d656f2664660fb3c00c1
nakashima1125 <free.nakashima@gmail.com>
2022-06-27 21:22:19 +0900

    add exception code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
+from django.http import Http404
 # データベースからデータをやり取り、そのデータを編集、整理する重要な役割です
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
@@ -15,3 +14,18 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+  # システム上にある最新の 5 件の質問項目をカンマで区切り、日付順に表示する
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list,}
+
+    return render(request, 'polls/index.html', context)
+
+# リクエストした ID を持つ質問が存在しないときに Http404 を送出
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
