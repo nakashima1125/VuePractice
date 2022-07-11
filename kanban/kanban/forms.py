25f0dcf1a3e91f9995c019f4146a89f2e5e74ec2
nakashima1125 <free.nakashima@gmail.com>
2022-07-11 20:58:02 +0900

    make

--- kanban/kanban/forms.py
+++ kanban/kanban/forms.py
@@ -1,25 +1,64 @@
-from dataclasses import field
 from django import forms
 from django.contrib.auth.models import User
 
 from .models import List, Card
 
+
 class UserForm(forms.ModelForm):
-  # Metaクラスにモデルと、指定したモデルのフィールドを指定
-  class Meta:
-    model = User
-    fields = ("username", "last_name", "first_name", "email")
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = User
+        fields = ("username", "last_name", "first_name", "email",)
 
 
 class ListForm(forms.ModelForm):
-  # リストのフォームにはtitleだけ指定。
-  # userまで指定すると、データベースに登録された全てのユーザーから選択できる状態になってしまう
-  # フォームの fields は 表示させる項目 と認識して、適切なフィールドを指定
-  class Meta:
-    model = List
-    fields = ("title",)
+
+    def __init__(self, *args, **kwargs):
+        super(ListForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = List
+        fields = ("title",)
+
 
 class CardForm(forms.ModelForm):
-  class Meta:
-    model = Card
-    fields = ("title", "description", "list")
+
+    def __init__(self, *args, **kwargs):
+        super(CardForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
+
+
+class CardCreateFromHomeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description",)
+
+
+class CardUpdateForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardUpdateForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+    class Meta:
+        model = Card
+        fields = ("title", "description", "list")
--- kanban/kanban/views.py
+++ kanban/kanban/views.py
@@ -1,22 +1,22 @@
 from django.contrib.auth import login
-from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
-from django.shortcuts import render, redirect, resolve_url
+from django.shortcuts import render, redirect, resolve_url, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, UpdateView, CreateView, ListView, DeleteView
 
-from .forms import UserForm, ListForm, CardForm
+from .forms import UserForm, ListForm, CardForm, CardCreateFromHomeForm 
 from .models import List, Card
 from .mixins import OnlyYouMixin
 
 def index(request):
     return render(request, "kanban/index.html")
 
-@login_required
-def home(request):
-    return render(request, "kanban/home.html")
+# @login_required
+# def home(request):
+#     return render(request, "kanban/home.html")
 
 
 def signup(request):
@@ -39,7 +39,6 @@ def signup(request):
 class UserDetailView(LoginRequiredMixin, DetailView):
     model = User
     template_name = "kanban/users/detail.html"
-
 
 class UserUpdateView(OnlyYouMixin, UpdateView): 
     model = User
@@ -77,20 +76,18 @@ class ListUpdateView(LoginRequiredMixin, UpdateView):
   model = List
   template_name = "kanban/lists/update.html"
   form_class = ListForm
-
-  def get_success_url(self):
-    return resolve_url("kanban:lists_detail", pk=self.kwargs['pk'])
+  success_url = reverse_lazy("kanban:home")
 
 class ListDeleteView(LoginRequiredMixin, DeleteView):
   model = List
   template_name = "kanban/lists/delete.html"
-  success_url = reverse_lazy("kanban:lists_list")
+  success_url = reverse_lazy("kanban:home")
 
 class CardCreateView(LoginRequiredMixin, CreateView):
   model = Card
   template_name = "kanban/cards/create.html"
   form_class = CardForm
-  success_url = reverse_lazy("kanban:cards_list")
+  success_url = reverse_lazy("kanban:home")
 
   def form_valid(self, form):
     form.instance.user = self.request.user
@@ -109,12 +106,29 @@ class CardUpdateView(LoginRequiredMixin, UpdateView):
   model = Card
   template_name = "kanban/cards/update.html"
   form_class = CardForm
-
-  def get_success_url(self):
-    return resolve_url("kanban:cards_detail", pk=self.kwargs['pk'])
+  success_url = reverse_lazy("kanban:home")
 
 class CardDeleteView(LoginRequiredMixin, DeleteView):
   model = Card
   template_name = "kanban/cards/delete.html"
-  success_url = reverse_lazy("kanban:cards_list")
-  
+  success_url = reverse_lazy("kanban:home")
+
+class HomeView(LoginRequiredMixin, ListView):
+  model = List
+  template_name = "kanban/home.html"
+
+
+class CardCreateFromHomeView(LoginRequiredMixin, CreateView):
+  model = Card
+  template_name = "kanban/cards/create.html"
+  form_class = CardCreateFromHomeForm
+  success_url = reverse_lazy("kanban:home")
+
+  def form_valid(self, form):
+    list_pk = self.kwargs["list_pk"]
+    # リストモデルのPKが list_pk のものを取得
+    # List.objects.get(pk=list_pk) との違いは、取得に失敗したときは404(Not Found)例外を送出する点
+    list_instance = get_object_or_404(List, pk=list_pk)
+    form.instance.list = list_instance
+    form.instance.user = self.request.user
+    return super().form_valid(form)
