b99b7683d89512ab791c2d8f5da7c7a6a70bf7c5
nakashima1125 <free.nakashima@gmail.com>
2022-07-26 06:56:23 +0900

    make create form

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from todo.models import TodoModel
 from .models import TodoModel
 
@@ -12,7 +12,13 @@ class Todolist(ListView):
 class TodoDetail(DetailView):
   template_name = 'detail.html'
   model = TodoModel
-  
+
+class TodoCreate(CreateView):
+  template_name = 'create.html'
+  # モデルの指定により、どこに保存すべきかを特定する
+  model = TodoModel
+  # form.as_pで表示される
+  fields = ('title', 'memo', 'priority', 'duedate')
 
 
 
