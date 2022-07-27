1787da5ab36ba91bfe5f257e18e364fb36f187a9
nakashima1125 <free.nakashima@gmail.com>
2022-07-28 07:14:13 +0900

    add post method

--- boardapp/views.py
+++ boardapp/views.py
@@ -2,5 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def signupfunc(request):
+  if request.method == "POST":
+    print('post')
   # {}はモデルのデータや任意のデータ
   return render(request, 'signup.html', {})
