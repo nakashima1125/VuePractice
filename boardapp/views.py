0d5e4c62e32bbb0ffc3858f1579d5f5f03c7f7bc
nakashima1125 <free.nakashima@gmail.com>
2022-07-29 06:51:44 +0900

    make user model

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,8 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+
 
 def signupfunc(request):
+  # 全てのUserデータをobjectへいれる
+  object = User.objects.get(username = 'nakashima')
+  print(object.email)
+
   if request.method == "POST":
     print('post')
+  else:
+    print('not post')
   # {}はモデルのデータや任意のデータ
   return render(request, 'signup.html', {})
