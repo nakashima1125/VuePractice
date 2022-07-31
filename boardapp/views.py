9352335220cf0a99b62ab7487a97ab4d42aca142
nakashima1125 <free.nakashima@gmail.com>
2022-08-01 06:59:01 +0900

    make list

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,7 +1,8 @@
 from django.db import IntegrityError
 from django.shortcuts import render
 from django.contrib.auth.models import User
-
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 def signupfunc(request):
   if request.method == "POST":
@@ -9,7 +10,24 @@ def signupfunc(request):
     password = request.POST['password']
     try:
       user = User.objects.create_user(username, '', password)
-    # エラー文からわかる 
     except IntegrityError:
       return render(request, 'signup.html', {'error' : "このユーザーは登録されています"})
   return render(request, 'signup.html', {})
+
+
+def loginfunc(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'login.html', {'context' : 'logged in'})
+    else:
+        return render(request, 'login.html', {'context' : 'not logged in'})
+  return render(request, 'login.html', {'context' : 'get method'})
+
+
+def listfunc(request):
+  return render(request, 'list.html', {})
+
