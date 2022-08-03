8b13edd4e30391796ab51c40527fe0b83cb95efd
nakashima1125 <free.nakashima@gmail.com>
2022-08-04 07:16:27 +0900

    ad read coded

--- boardapp/views.py
+++ boardapp/views.py
@@ -56,3 +56,18 @@ def goodfunc(request, pk):
   object.good = object.good + 1
   object.save()
   return redirect('list')
+
+
+def readfunc(request, pk):
+  object = BoardModel.objects.get(pk=pk)
+  # loginしているusernameを取得
+  username = request.user.get_username()
+  # すでに既読した者の中にログインしているユーザーを含んでいるかどうか
+  if username in object.readtext:
+    return redirect('list')
+  else:
+    object.read = object.read + 1
+  # 既読したユーザーを付け足していく（空白で挟む、しかし、同じ苗字などの対策はしていない）
+    object.readtext = object.readtext + '' + username
+    object.save()
+    return redirect('list')
