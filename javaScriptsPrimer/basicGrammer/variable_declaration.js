f1ebeeb0ff7167b54c5d32a7b2c1c5a609f3a46a
nakashima1125 <free.nakashima@gmail.com>
2023-09-04 12:00:49 +0900

    変数と宣言コードを追加

---
+++ javaScriptsPrimer/basicGrammer/variable_declaration.js
@@ -0,0 +1,18 @@
+// constは再代入できない変数の宣言。必ずしも定数で定義されているわけではない。
+const test = "value";
+const booktitle = "みんなのpython";
+
+// letは再代入可能な変数の宣言 
+let bookName = "情報処理安全確保支援士入門";
+
+/* 
+letは再代入可能な変数の宣言。
+varは意図せずに変数を再定義してできてしまう欠点がある。
+使用は推奨しない。
+*/
+var BOOKTITLE = "データベーススペシャリスト入門";
+
+console.log(test);
+console.log(booktitle);
+console.log(bookName);
+console.log(BOOKTITLE);
