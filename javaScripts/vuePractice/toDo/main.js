c2b1234ece2d9d3cdab165dd98131a6cd409d7e9
nakashima1125 <free.nakashima@gmail.com>
2023-09-12 10:48:09 +0900

    add doAdds of method

--- javaScripts/vuePractice/toDo/main.js
+++ javaScripts/vuePractice/toDo/main.js
@@ -20,5 +20,25 @@ const app = new Vue({
   data: {
     todos: [], //空の配列
   },
-  method: {},
+  method: {
+    //   todo処理の追加
+    doAdd: function (event, value) {
+      // refで名前を付けておいた要素を参照
+      var comment = this.$refs.comment;
+      // 入力がなければ何もしないでreturn
+      if (!comment.value.length) {
+        return;
+      }
+      // { 新しいID, コメント, 作業状態 }
+      // というオブジェクトを現在の todos リストへ push
+      // 作業状態「state」はデフォルト「作業中=0」で作成
+      this.todos.push({
+        id: todoStorage.uid++,
+        comment: comment.value,
+        state: 0,
+      });
+      //   フォーム要素を空にする。
+      comment.value = "";
+    },
+  },
 });
