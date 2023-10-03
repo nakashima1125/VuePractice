68180d5b426bf968575d1aed9d8826c906c705cd
nakashima1125 <free.nakashima@gmail.com>
2023-10-03 18:02:27 +0900

    test

--- javaScripts/vuePractice/hanson/src/App.vue
+++ javaScripts/vuePractice/hanson/src/App.vue
@@ -4,26 +4,34 @@ import draggable from "vuedraggable";
 const data = [
   {
     id: 1,
+    name: "キャベツ",
     content: "テスト1",
+    categoryMo: "1",
   },
   {
     id: 2,
+    name: "赤い",
     content: "テスト2",
+    categoryNo: "2",
   },
   {
     id: 3,
+    name: "wswsw",
     content: "テスト3",
+    categoryMo: "3",
   },
 ];
 </script>
 
 <template>
-  <div class="hello">
+  <div>
+    <!-- .handle クラスが指定された要素は、その部分をクリックして要素をドラッグできるハンドルとして機能する -->
     <draggable v-model="data" group="people" item-key="id" handle=".handle">
       <template #item="{ element }">
         <div class="drag-item">
-          <span class="handle">ここを押せば動かせます。</span>
-          {{ element.id }}
+          <li class="handle">
+            ここを押せば動かせます。 {{ element.id }}-{{ element.content }}
+          </li>
         </div>
       </template>
     </draggable>
@@ -33,6 +41,15 @@ const data = [
 <style scoped>
 .drag-item {
   background: rgb(233, 249, 255);
-  margin: 10px 0;
+  margin: 10px;
+  width: 500px;
+}
+li {
+  cursor: pointer;
+  padding: 10px;
+  border: solid #ddd 1px;
+}
+ul {
+  list-style-type: none;
 }
 </style>
