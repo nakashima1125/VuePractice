f4b6c284c778ec184c8757bad2d7bfd5bccee936
cmb-sy <free.nakashima@gmail.com>
2024-02-28 17:43:13 +0900

    update

---
+++ typeScript/LearnMemo/ternaryOperator.js
@@ -0,0 +1,8 @@
+/*
+3項演算子
+*/
+var age = 20;
+var test1 = age >= 20 ? true : false;
+console.log("test1", test1);
+var test2 = !(age > 20);
+console.log("test2", test2);
---
+++ typeScript/LearnMemo/ternaryOperator.ts
@@ -0,0 +1,6 @@
+const age = 20;
+const test1 = age >= 20 ? true : false;
+console.log("test1", test1);
+
+const test2 = !(age > 20);
+console.log("test2", test2);
