c289e5654c13244eef4dda8adb8c1de8f62cb2ff
cmb-sy <free.nakashima@gmail.com>
2024-03-28 10:18:28 +0900

    update

---
+++ typeScript/サバイバルTypeScript/作って学ぶTypeScript/README.md
@@ -0,0 +1,2 @@
+本コンテンツは以下のレポジトリへ移動しました。それに加え魔改造しています。<br>
+https://github.com/cmb-sy/cat-nyanko-Generator
--- typeScript/サバイバルTypeScript/作って学ぶTypeScript/increment.js
+++
@@ -1,6 +0,0 @@
-function increment(num) {
-  return num + 1;
-}
-
-console.log(increment(999));
-console.log(increment("999"));
