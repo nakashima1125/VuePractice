d7e299bc0fd32449b308c4878cd6a09dbd042418
cmb-sy <free.nakashima@gmail.com>
2024-04-02 17:16:20 +0900

    update

--- javaScript/reactGettingStartedGuide/react-guide-material/03_js_basic/050_destructuring/start/main.js
+++ javaScript/reactGettingStartedGuide/react-guide-material/03_js_basic/050_destructuring/start/main.js
@@ -1,26 +1,35 @@
-const arry = ["配列1", "配列2", "配列3"];
-console.log(arry[0]);
-console.log(arry[2]);
+// const arry = ["配列1", "配列2", "配列3"];
+// const [a, , c] = ["配列1", "配列2", "配列3"];
+// console.log(a);
+// console.log(c);
 
-const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
-console.log(obj.x);
-console.log(obj.y);
+// const obj = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+// const { x, z } = { x: "オブジェクト1", y: "オブジェクト2", z: "オブジェクト3" };
+// console.log(obj.x);
+// console.log(x);
+// console.log(z);
 
 const arr = ["Japan", "Tokyo", "Shinjuku"];
 const objAddress = { country: "Japan", state: "Tokyo", city: "Shinjuku" };
-
-const fnArr = (arry) => {
+const fnArr = ([country, prefecture, city]) => {
   console.log("---配列---");
-  console.log(`country: ${arry[0]}`);
-  console.log(`state: ${arry[1]}`);
-  console.log(`city: ${arry[2]}`);
+  console.log(`country: ${country}`);
+  console.log(`state: ${prefecture}`);
+  console.log(`city: ${city}`);
+  // const fnArr = (arry) => {
+  // console.log(`country: ${arry[0]}`);
+  // console.log(`state: ${arry[1]}`);
+  // console.log(`city: ${arry[2]}`);
 };
 
-const fnObj = (objAddr) => {
+const fnObj = ({ country, state, city }) => {
   console.log("---オブジェクト---");
-  console.log(`country: ${objAddr.country}`);
-  console.log(`state: ${objAddr.state}`);
-  console.log(`city: ${objAddr.city}`);
+  console.log(`country: ${country}`);
+  console.log(`city: ${city}`);
+  // const fnObj = (objAddr) => {
+  // console.log(`country: ${objAddr.country}`);
+  // console.log(`state: ${objAddr.state}`);
+  // console.log(`city: ${objAddr.city}`);
 };
 
 fnArr(arr);
