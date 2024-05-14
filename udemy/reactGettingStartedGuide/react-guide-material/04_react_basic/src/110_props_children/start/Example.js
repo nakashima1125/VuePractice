cf6d1e9c19105e61ed07714df64900cff338bc81
cmb-sy <free.nakashima@gmail.com>
2024-05-14 10:21:03 +0900

    update

--- udemy/reactGettingStartedGuide/react-guide-material/04_react_basic/src/110_props_children/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/04_react_basic/src/110_props_children/start/Example.js
@@ -2,14 +2,27 @@ import Profile from "./components/Profile";
 import Container from "./components/Container";
 
 const profile = [
-  { name: "Takashi", age: 19, country: "Japan", color: "green" },
-  { name: "Jane", age: 28, country: "UK", color: "blue" },
+  { name: "Takashi", age: 19, country: "Japan" },
+  { name: "Jane", age: 28, country: "UK", color: "red" },
 ];
 
 const Example = () => {
   return (
     <div>
-      <Container title="Childrenとは？" />
+      <Container title="順番通りに渡す">
+        <Profile {...profile[0]} />
+        <Profile {...profile[1]} />
+      </Container>
+
+      <Container
+        title="個別に渡す"
+        children={[
+          <Profile key={profile[0].name} {...profile[0]} />,
+          <Profile key={profile[1].name} {...profile[1]} />,
+        ]}
+        first={<Profile key={profile[0].name} {...profile[0]} />}
+        second={<Profile key={profile[1].name} {...profile[1]} />}
+      />
     </div>
   );
 };
