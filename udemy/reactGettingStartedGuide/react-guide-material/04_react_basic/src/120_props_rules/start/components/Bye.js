9767ac67c55f50abe94a68b7825f16051f9a6a6a
cmb-sy <free.nakashima@gmail.com>
2024-05-22 09:55:41 +0900

    update

--- udemy/reactGettingStartedGuide/react-guide-material/04_react_basic/src/120_props_rules/start/components/Bye.js
+++ udemy/reactGettingStartedGuide/react-guide-material/04_react_basic/src/120_props_rules/start/components/Bye.js
@@ -1,4 +1,6 @@
 const Bye = (props) => {
+  const desc = Reflect.getOwnPropertyDescriptor(props, "name");
+  console.log(desc);
   return (
     <div>
       <h3>Bye {props.name}</h3>
--- udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,16 @@
 const Example = () => {
+  const clickHandlier = () =>{
+    alert('クリックされた')
+  }
+  const clickHandlier2 = () =>{
+    console.log("ボタンがクリックされました")
+  }
   return (
-    <></>
+    <>
+    <button onClick={clickHandlier}>クリックしてね</button>
+    <button onClick={clickHandlier2}>クリックしてね</button>
+    <button onClick={() =>{alert('test')}}>クリックしてね</button>
+    </>
   );
 };
 
