de8669f0667fc8aee3c8453bc82e65430a3d51ce
cmb-sy <free.nakashima@gmail.com>
2024-05-27 10:22:29 +0900

    learn state of object

--- udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/060_state_object/start/Example.js
@@ -2,6 +2,26 @@ import { useState } from "react";
 
 const Example = () => {
   const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj)
+  const chgName =(e) =>{setPerson({name: e.target.value, age:person.age})}
+  const chgAge =(e) =>{
+    setPerson({...person, age:e.target.value})}
+  // const chgAge =(e) =>{setPerson({name: person.name, age:e.target.value})}
+  const reset = () =>{
+    setPerson({name:"", age:""})
+  }
+
+  return(
+    <>
+    <h3>Name:{person.name}</h3>
+    <h3>Age:{person.age}</h3>
+    <input type="text" value={person.name} onChange={chgName}></input>
+    <input type="number" value={person.age} onChange={chgAge}></input>
+    <div>
+      <button onClick={reset}>リセット</button>
+    </div>
+    </>
+  )
 };
 
 export default Example;
