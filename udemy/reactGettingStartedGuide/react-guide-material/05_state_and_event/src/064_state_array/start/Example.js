676a05d7044f0780cb4d2ceffd9c41b2b218c1aa
cmb-sy <free.nakashima@gmail.com>
2024-05-29 09:45:41 +0900

    update

--- udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/064_state_array/start/Example.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const Example = () => {
   const numArray = [1, 2, 3, 4, 5];
-
+  const [nums, setNums] = useState(numArray)
+  const shuffle =() =>{
+    const newNums = [...nums]
+    // 末尾を取り出す
+    const popValue = newNums.pop()
+    newNums.unshift(popValue)
+    setNums(newNums)
+  }
+  // console.log(nums)
   return (
     <>
-      
+      <h1>
+      {nums}
+      </h1>
+      <button onClick={shuffle}>shuffle</button>
     </>
   );
 };
--- udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/05_state_and_event/src/070_state_and_component/start/Example.js
@@ -1,7 +1,26 @@
 import { useState } from "react";
 
-const Example = () => {
-  const [count, setCount] = useState(0);
+const Example =(_)=>{
+  const [toggle, setToggle] = useState(true)
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
+  const toggleComponet = () =>{
+    setToggle(prev => !prev)
+  }
+  return(
+  <>
+  <button onClick={toggleComponet}>toggle</button>
+  {toggle ? <Count key="A" title="A" count={countA} setCount={setCountA}/> 
+  : <Count key='B' title="B" count={countB} setCount={setCountB}/> }  
+  {/* <Count key="A" title="A" count={countA} setCount={setCountA}/> 
+  <Count key='B' title="B" count={countB} setCount={setCountB}/>   */}
+  {/* <Count title="A" />
+  <Count title="B" /> */}
+  </>
+  )
+}
+
+const Count = ({title, count, setCount}) => {
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
   };
@@ -10,7 +29,7 @@ const Example = () => {
   };
   return (
     <>
-      <h3>カウント: {count}</h3>
+      <h3>{title}: {count}</h3>
       <button onClick={countUp}>+</button>
       <button onClick={countDown}>-</button>
     </>
