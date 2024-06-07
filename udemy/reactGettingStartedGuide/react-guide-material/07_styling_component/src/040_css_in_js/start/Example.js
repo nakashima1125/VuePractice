19c9790d058937361aa30625ae2072c4ac4433f8
cmb-sy <free.nakashima@gmail.com>
2024-06-07 10:01:37 +0900

    update

--- udemy/reactGettingStartedGuide/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/07_styling_component/src/040_css_in_js/start/Example.js
@@ -1,4 +1,33 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+console.dir(styled);
+const StyledButton = styled.button`
+  margin: 10px auto;
+  border-radius: 9999px;
+  border: none;
+  display: block;
+  width: 120px;
+  height: 60px;
+  font-weight: bold;
+
+  cursor: pointer;
+  background-color: ${({ isSelected }) => (isSelected ? `pink` : ``)};
+`;
+
+const OrangeButton = styled(StyledButton)`
+  background-color: orange;
+  :hover,
+  :active {
+    color: red;
+    opacity: 0.7;
+    transform: scale(1.1);
+    transition: all 0.3s ease-out 0s;
+    @media (max-width: 600px) {
+      border-radius: 0;
+    }
+  }
+`;
 
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
@@ -7,9 +36,12 @@ const Example = () => {
 
   return (
     <>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
+      <StyledButton isSelected={isSelected} onClick={clickHandler}>
         ボタン
-      </button>
+      </StyledButton>
+      <OrangeButton isSelected={isSelected} onClick={clickHandler}>
+        ボタン
+      </OrangeButton>
       <div style={{ textAlign: "center" }}>
         {isSelected && "クリックされました。"}
       </div>
