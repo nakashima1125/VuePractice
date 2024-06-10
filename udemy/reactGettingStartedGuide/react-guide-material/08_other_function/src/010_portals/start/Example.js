ba6ee0a134e4e688e0c9cbe7c42c8bbc4566c938
cmb-sy <free.nakashima@gmail.com>
2024-06-10 10:26:29 +0900

    update

--- udemy/reactGettingStartedGuide/react-guide-material/08_other_function/src/010_portals/start/Example.js
+++ udemy/reactGettingStartedGuide/react-guide-material/08_other_function/src/010_portals/start/Example.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { Children, useState } from "react";
 import { createPortal } from "react-dom";
 import Modal from "./components/Modal";
 
@@ -14,11 +14,16 @@ import Modal from "./components/Modal";
 モーダル、ポップアップ、トーストは使用の代表例です。
 */
 
+const ModalPotal = ({ children }) => {
+  const target = document.querySelector(".container.start");
+  return createPortal(children, target);
+};
+
 const Example = () => {
   const [modalOpen, setModalOpen] = useState(false);
   return (
-    <div>
-      <div className="container start"></div>
+    <div onClick={() => console.log("空のdiv")}>
+      <div className="container start" onClick={() => console.log("div")}></div>
 
       <button
         type="button"
@@ -27,7 +32,11 @@ const Example = () => {
       >
         モーダルを表示する
       </button>
-      {modalOpen && <Modal handleCloseClick={() => setModalOpen(false)} />}
+      {modalOpen && (
+        <ModalPotal>
+          <Modal handleCloseClick={() => setModalOpen(false)} />
+        </ModalPotal>
+      )}
     </div>
   );
 };
